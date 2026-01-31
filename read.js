69a7c3be6675db4e873262be075d71338fa42eae
Aman KrSharma <amankrsharma2011@gmail.com>
2026-01-31 12:17:25 +0530

    Update read.js

--- read.js
+++ read.js
@@ -1,4 +1,1 @@
-var url = new URLSearchParams(window.location.search);
-var name = url.get("username");
-var place = url.get("continent")
-document.getElementByld("result").innerText = "So, your name is " + name + " and you are from " + place + ".";
+
